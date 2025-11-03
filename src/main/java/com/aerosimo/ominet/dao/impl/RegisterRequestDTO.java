8159d801948a58bf2a9bd7fc7638cc4854672a45
Elijah Omisore <elijah@omisore.co.uk>
2025-11-03 01:21:21 +0000

    [AER03112025] Testing for early bugs

---
+++ src/main/java/com/aerosimo/ominet/dao/impl/RegisterRequestDTO.java
@@ -0,0 +1,81 @@
+/******************************************************************************
+ * This piece of work is to enhance authcore project functionality.           *
+ *                                                                            *
+ * Author:    eomisore                                                        *
+ * File:      RegisterRequestDTO.java                                    *
+ * Created:   01/11/2025, 14:40                                               *
+ * Modified:  01/11/2025, 14:40                                               *
+ *                                                                            *
+ * Copyright (c)  2025.  Aerosimo Ltd                                         *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included    *
+ * in all copies or substantial portions of the Software.                     *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,            *
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES            *
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                   *
+ * NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT                 *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,               *
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING               *
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE                 *
+ * OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.aerosimo.ominet.dao.impl;
+
+public class RegisterRequestDTO {
+
+    public String username;
+    public String email;
+    public String password;
+
+    public RegisterRequestDTO() {
+    }
+
+    public RegisterRequestDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterRequestDTO{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
